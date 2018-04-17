408b8a44b25cae16c9c8b9b702e38cd8501567a0
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-04-17 00:20:15 -0500

    Implemented log in functionality with JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
 const router = express.Router();
 
 const keys = require('../../config/keys');
@@ -95,6 +97,17 @@ router.post("/login", (req, res) => {
         return res.status(404).json({ password: 'Password incorrect' });
       }
     });
+  });
+});
+
+// @route GET api/users/register
+// @desc Return current user
+// @access Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
   });
 });
 
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require('passport');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+// Load Route APIs
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
 const posts = require("./routes/api/posts");
-
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -20,15 +20,20 @@ mongoose
   .then(() => console.log("MongoDB Connected!"))
   .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/profiles", profiles);
 app.use("/api/posts", users);
 
+// App Listening
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
