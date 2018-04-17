3e4aa117e79cb22773b585791a9daa6e14d2b006
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-04-16 21:39:15 -0500

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
