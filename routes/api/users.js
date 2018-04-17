e4caa16681a9312357c22f902774966987a53395
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-04-16 23:50:24 -0500

    Create user login api

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,9 +15,11 @@ router.get("/test", (req, res) => res.json({ message: "Hello world!" }));
 // @desc Register route
 // @access Public
 router.post("/register", (req, res) => {
+  // Find User By Email
   User.findOne({
     email: req.body.email
   }).then(user => {
+    // Check For User
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
@@ -48,6 +50,33 @@ router.post("/register", (req, res) => {
       });
 
     }
+  });
+});
+
+// @route POST api/users/login
+// @desc Login route / Returning JWT Token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find User By Email
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    // Check For User
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(404).json({ password: 'Password incorrect' });
+      }
+    });
   });
 });
 
