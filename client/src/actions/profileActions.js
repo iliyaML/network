b016787ee6f9a4d0c6e2714a333622945e3580ef
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-04-27 12:04:42 -0500

    Added create profile functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,15 +5,28 @@ import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
     dispatch(setProfileLoading());
-    axios.get('/api/profile')
-    .then(res => dispatch({
-        type: GET_PROFILE,
-        payload: res.data
-    }))
-    .catch(err => dispatch({
-        type: GET_PROFILE,
-        payload: {}
-    }));
+    axios.get('/api/profiles')
+        .then(res => dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        }))
+        .catch(err => dispatch({
+            type: GET_PROFILE,
+            payload: {}
+        }));
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profiles', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
 };
 
 // Profile loading
