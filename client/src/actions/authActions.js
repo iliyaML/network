0338071338a71f191200f03669142f0f3b9672ad
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-04-26 23:39:27 -0500

    Login implemented...

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -9,4 +12,34 @@ export const registerUser = (userData, history) => dispatch => {
             type: GET_ERRORS,
             payload: err.response.data
         }));
+};
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+    .then(res => {
+        // Save to LocalStorage
+        const { token } = res.data;
+        // Set token to LS
+        localStorage.setItem('jwtToken', token);
+        // Set token to Auth header
+        setAuthToken(token);
+        // Decode token to get user data
+        const decoded = jwt_decode(token);
+        // Set current user
+        dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        });
+    });
+};
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
 };
