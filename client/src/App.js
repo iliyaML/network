b5072cec264274d7c1dd0fcba2ad355ef905f174
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-04-26 22:51:26 -0500

    More redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -13,7 +14,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Provider>
+      <Provider store={store}>
         <Router>
           <div className="App">
             <Navbar />
