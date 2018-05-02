8f78e3e081cf9cae8cc606a9693ca42720b89f83
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-05-02 16:33:43 -0500

    Added 404 route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -55,35 +55,24 @@ class App extends Component {
         <Router>
           <div className="App">
             <Navbar />
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:handle" component={Profile} />
-              <Switch>
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <div className="container">
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/profiles" component={Profiles} />
+                <Route exact path="/profile/:handle" component={Profile} />
+                <Route exact path="/not-found" component={NotFound} />
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              </Switch>
-              <Switch>
                 <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-              </Switch>
-              <Switch>
                 <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-              </Switch>
-              <Switch>
                 <PrivateRoute exact path="/add-experience" component={AddExperience} />
-              </Switch>
-              <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
-              </Switch>
-              <Switch>
                 <PrivateRoute exact path="/feed" component={Posts} />
-              </Switch>
-              <Switch>
                 <PrivateRoute exact path="/post/:id" component={Post} />
-              </Switch>
-              <Route exact path="/not-found" component={NotFound} />
-            </div>
+                <Redirect from='*' to='/not-found' />
+              </div>
+            </Switch>
             <Footer />
           </div>
         </Router>
